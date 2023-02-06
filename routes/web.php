3c5c227241ac0de5617c13e5c474ef59c7ecff15
R-t0923 <t.07r.12@gmail.com>
2023-02-07 01:07:53 +0900

    inertia-vue.js

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\InertiaTestController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/inertia-test', function () {
+    return Inertia::render('InertiaTest');
 });
+
+Route::get('/component-test', function () {
+    return Inertia::render('ComponentTest');
+});
+
+
+Route::controller(InertiaTestController::class)->prefix('inertia')->name('inertia.')->group(function(){
+    Route::get('/index', 'index')->name('index');
+    Route::get('/show/{id}', 'show')->name('show');
+    Route::get('/create', 'create')->name('create');
+    Route::post('/', 'store')->name('store');
+    Route::delete('/{id}', 'delete')->name('delete');
+});
+
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
